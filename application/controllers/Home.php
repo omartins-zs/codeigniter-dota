07706140151f3d4de36f164ef94afc47a55f2ee6
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-15 15:01:35 -0300

    Primeiros Ajustes No Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$dados['titulo'] = "Site CI Dota";
+		
+		$this->load->view('templates/header', $dados);
+		$this->load->view('templates/headernav-top', $dados);
 		$this->load->view('pages/index', $dados);
+		$this->load->view('templates/footer', $dados);
+		$this->load->view('templates/js', $dados);
 	}
 }
