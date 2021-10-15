9aada32c4f6fe6adec4da2db3b0e528fb56d9879
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-15 12:05:34 -0300

    Adicionando template Header

---
+++ application/views/templates/header.php
@@ -0,0 +1,22 @@
+<?php
+
+$dir = "";
+if($_SERVER['SERVER_NAME'] == "sitedozero.com.br"){
+    $dir = "/";
+}else if($_SERVER['SERVER_NAME'] == "localhost"){
+    $dir = "/local/codedota/";
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <base href="//<?=$_SERVER['SERVER_NAME'].$dir?> ">
+    <title><?= $titulo ?></title>
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+
+<body style="background-color: #010101">
