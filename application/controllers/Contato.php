d574f823c5348b6ad89698efbb0b534c91c246fb
omartins-zs <gabrielmatheus209@gmail.com>
2021-11-17 16:49:38 -0300

    Alterando Nome de visualizado do Controller

--- application/controllers/Contato.php
+++ application/controllers/Contato.php
@@ -1,15 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Contato extends CI_Controller {
 
 	public function index()
 	{
-		$dados['titulo'] = "Home - CI Dota";
+		$dados['titulo'] = "Contato - CI Dota";
 		
 		$this->load->view('templates/header', $dados);
 		$this->load->view('templates/nav-top', $dados);
-		$this->load->view('pages/index', $dados);
+		$this->load->view('pages/contato', $dados);
 		$this->load->view('templates/footer', $dados);
 		$this->load->view('templates/js', $dados);
 	}
