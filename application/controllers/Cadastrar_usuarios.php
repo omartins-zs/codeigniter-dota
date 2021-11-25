b2e0e1c955ecf57a54e10df0fffc009babdad184
omartins-zs <gabrielmatheus209@gmail.com>
2021-11-25 17:03:26 -0300

    Criando Function novo usuario

--- application/controllers/Cadastrar_usuarios.php
+++ application/controllers/Cadastrar_usuarios.php
@@ -1,16 +1,29 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Cadastrar_usuarios extends CI_Controller {
+class Cadastrar_usuarios extends CI_Controller
+{
 
 	public function index()
 	{
 		$dados['titulo'] = "Cadastro de Usuários - CI Dota";
-		
+
 		$this->load->view('templates/header', $dados);
 		$this->load->view('templates/nav-top', $dados);
 		$this->load->view('pages/cadastro_usuarios', $dados);
 		$this->load->view('templates/footer', $dados);
 		$this->load->view('templates/js', $dados);
-	}	
+	}
+
+	public function novo()
+	{
+		$usuario = array(
+			"nome" => $this->input->post("nome"),
+			"email" => $this->input->post("email"),
+			"username" => $this->input->post("username"),
+			"senha" => $this->input->post("senha")
+		);
+		$this->load->model('usuarios_model');
+		$this->usuarios_model->cadastra_usuario($usuario);
+	}
 }
