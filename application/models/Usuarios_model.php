6d4f6cafd87966179019f4917f5d9725452e161f
omartins-zs <gabrielmatheus209@gmail.com>
2021-11-26 14:39:08 -0300

    Criando Function LogarUsuarios

--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -1,7 +1,17 @@
-<?php 
+<?php
 
-class Usuarios_model extends CI_Model {
-    public function cadastra_usuario($usuario){
-        $this->db->insert("usuarios_tb",$usuario);
+class Usuarios_model extends CI_Model
+{
+    public function cadastra_usuario($usuario)
+    {
+        $this->db->insert("usuarios_tb", $usuario);
+    }
+
+    public function logarUsuarios($username, $senha)
+    {
+        $this->db->where("username", $username);
+        $this->db->where("senha", $senha);
+        $usuario = $this->db->get("usuarios_tb")->row_array();
+        return $usuario;
     }
 }
